3469d98e02ca784e316ef8d8586cf998aa7f13a4
prakruti-pathak <prakrutipathak109@gmail.com>
2024-11-22 13:17:49 +0530

    Create demo lambda function which take json input and give output in uppercase

---
+++ AWSLambda/Function.cs
@@ -0,0 +1,26 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSLambda;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
+    public string FunctionHandler(User input, ILambdaContext context)
+    {
+        return input.Name.ToUpper();
+    }
+    public  class User
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; }
+    }
+}
