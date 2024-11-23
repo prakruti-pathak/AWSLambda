424844234de89aa2a77a2541ac47909d698b2aa9
prakruti-pathak <prakrutipathak109@gmail.com>
2024-11-23 14:28:25 +0530

    Done with CRUD operation

--- AWSLambda/obj/Debug/net8.0/AWSLambda.AssemblyInfo.cs
+++ AWSLambda/obj/Debug/net8.0/AWSLambda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AWSLambda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3469d98e02ca784e316ef8d8586cf998aa7f13a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba3fd4316bae11b8bf686ec7ed9d064f248845b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AWSLambda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AWSLambda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
